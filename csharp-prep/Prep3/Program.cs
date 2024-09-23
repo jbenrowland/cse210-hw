b35ac9c9782c89394389ae174bc9945ee6697be4
jbenrowland <jbenrowland14@gmail.com>
2024-09-23 14:09:04 -0600

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,46 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
-
-// Notes 
-
-// C#
-// string response = "yes";
-
-// while (response == "yes")
-// {
-//     Console.Write("Do you want to continue? ");
-//     response = Console.ReadLine();
-// }
-
-// Do-While Loops
-// string response;
-
-// do
-// {
-//     Console.Write("Do you want to continue? ");
-//     response = Console.ReadLine();
-// } while (response == "yes");
-
-//For Loops
-// for (int i = 0; i < 10; i++)
-// {
-//     Console.WriteLine(i);
-// }
-// for (int i = 2; i <= 20; i = i + 2)
-// {
-//     Console.WriteLine(i);
-// }
-
-//Foreach Loops
-//foreach (string color in colors)
-// {
-//     Console.WriteLine(color);
-// }
-
-//Random Numbers
-//Random randomGenerator = new Random();
-// int number = randomGenerator.Next(1, 11);
