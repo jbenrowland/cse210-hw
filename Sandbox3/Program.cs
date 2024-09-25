2cac395d0ec0b7d1f82a7fbe999b2bae88007551
jbenrowland <jbenrowland14@gmail.com>
2024-09-25 13:12:24 -0600

    SANBOX!!

--- Sandbox3/Program.cs
+++ Sandbox3/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> x;
+        x = ["twenty-four", "twenty-five","twenty-six"] ;
+
+        Console.WriteLine("Hello Sandbox World!");
+
+        foreach (number in x)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+}
+
